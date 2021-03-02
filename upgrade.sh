2cc4181cc7b66e19a17513316ebdbf1ff78539bf
281677160 <66651997+281677160@users.noreply.github.com>
2021-03-03 01:46:43 +0800

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -144,7 +144,7 @@ Diy_Part3() {
 		AutoBuild_Firmware="${COMP1}-${Openwrt_Version}.${Firmware_sfx}"
 		AutoBuild_Detail="${COMP1}-${Openwrt_Version}.detail"
 		echo "Firmware: ${AutoBuild_Firmware}"
-		 ${Firmware_Path}/${Default_Firmware} bin/Firmware/${AutoBuild_Firmware}
+		cp ${Firmware_Path}/${Default_Firmware} bin/Firmware/${AutoBuild_Firmware}
 		_MD5=$(md5sum bin/Firmware/${AutoBuild_Firmware} | cut -d ' ' -f1)
 		_SHA256=$(sha256sum bin/Firmware/${AutoBuild_Firmware} | cut -d ' ' -f1)
 		echo -e "\nMD5:${_MD5}\nSHA256:${_SHA256}" > bin/Firmware/${AutoBuild_Detail}
