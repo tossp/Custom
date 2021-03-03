e543ee757f27ba94d5e2e0ad3fc3c8159c92d53e
281677160 <66651997+281677160@users.noreply.github.com>
2021-03-03 10:34:10 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -181,7 +181,7 @@ if [[ ${REGULAR_UPDATE} == "true" ]]; then
 	echo " 固件版本: ${Openwrt_Version}"
 	echo " 云端路径: ${Github_UP_RELEASE}"
 	echo " 《编译成功，会自动把固件发布到指定地址，然后才会生成云端路径》"
-	echo " 《请把“REPO_TOKEN”密匙设置好,没设置好密匙不能发布云端地址》"
+	echo " 《请把“REPO_TOKEN”密匙设置好,没设置好密匙不能发布就生成不了云端地址》"
 	echo " 《x86-64、phicomm_k2p、phicomm-k3、newifi-d2已自动适配固件名字跟后缀，无需自行设置》"
 	echo " 《如有其他机子可以用定时更新固件的话，请告诉我，我把固件名字跟后缀适配了》"
 	echo ""
