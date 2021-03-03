74dabb17cdd272aa96bef63db1252febf7f703cd
281677160 <66651997+281677160@users.noreply.github.com>
2021-03-03 10:14:24 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -120,11 +120,11 @@ echo ""
 Diy_xinxi_Base() {
 GET_TARGET_INFO
 if [[ "${TARGET_PROFILE}" =~ (x86-64|phicomm-k3|xiaomi_mir3g|d-team_newifi-d2|phicomm_k2p|k2p) ]]; then
-	Firmware_mz=" 自动适配"
-	Firmware_hz=" 自动适配"
+	Firmware_mz="自动适配"
+	Firmware_hz="自动适配"
 else
-	Firmware_mz=" ${Up_Firmware}"
-	Firmware_hz=" ${Firmware_sfx}"
+	Firmware_mz="${Up_Firmware}"
+	Firmware_hz="${Firmware_sfx}"
 fi
 echo ""
 echo " 编译源码: ${COMP2}"
