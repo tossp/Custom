97f10bde4cfba0c0d0c025547bd0fdda7c65ef22
281677160 <66651997+281677160@users.noreply.github.com>
2021-03-05 18:37:37 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -50,6 +50,7 @@ Diy_lede2() {
 echo "LEDE源码自定义2"
 rm -rf feeds/packages/lang/golang
 svn co https://github.com/Lienol/openwrt-packages/trunk/lang/golang feeds/packages/lang/golang
+./scripts/feeds install -a
 }
 
 ################################################################################################################
