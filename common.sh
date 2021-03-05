ea84c00081768dae3f019aeea8fb5f7e6025619a
281677160 <66651997+281677160@users.noreply.github.com>
2021-03-05 10:46:16 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -4,16 +4,13 @@
 # matrix.target=${Modelfile}
 
 # 全脚本源码通用diy.sh文件
-
 Diy_all() {
 echo "all"
 git clone https://github.com/281677160/luci-app-autoupdate package/luci-app-autoupdate
-mv "$PATH1/{AutoUpdate.sh,AutoBuild_Tools.sh} package/base-files/files/bin
+mv "${PATH1}"/{AutoUpdate.sh,AutoBuild_Tools.sh} package/base-files/files/bin
 chmod -R +x package/base-files/files/bin
 }
-
 # 全脚本源码通用diy2.sh文件
-
 Diy_all2() {
 echo "all2"
 git clone https://github.com/openwrt-dev/po2lmo.git
@@ -26,29 +23,29 @@ rm -rf ./*/*/{LICENSE,README,README.md}
 }
 
 ################################################################################################################
-
-
 # LEDE源码通用diy1.sh文件
-
+################################################################################################################
 Diy_lede() {
 echo "LEDE源码自定义1"
 cp -Rf build/common/LEDE/* "${PATH1}"
 git clone -b $REPO_BRANCH --single-branch https://github.com/281677160/openwrt-package package/danshui
 rm -rf package/lean/{luci-app-netdata,luci-theme-argon,k3screenctrl}
-
+if [[ "${Modelfile}" == "Lede_x86_64" ]]; then
+sed -i '/IMAGES_GZIP/d' "${PATH1}/${CONFIG_FILE}" > /dev/null 2>&1
+echo -e "\nCONFIG_TARGET_IMAGES_GZIP=y" >> "${PATH1}/${CONFIG_FILE}"
+fi
 git clone https://github.com/fw876/helloworld package/danshui/luci-app-ssr-plus
 git clone https://github.com/xiaorouji/openwrt-passwall package/danshui/luci-app-passwall
 git clone https://github.com/jerrykuku/luci-app-vssr package/danshui/luci-app-vssr
 git clone https://github.com/vernesong/OpenClash package/danshui/luci-app-openclash
 git clone https://github.com/frainzy1477/luci-app-clash package/danshui/luci-app-clash
 git clone https://github.com/garypang13/luci-app-bypass package/danshui/luci-app-bypass
-
 find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-redir/shadowsocksr-libev-alt/g' {}
 find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-server/shadowsocksr-libev-server/g' {}
 }
-
+################################################################################################################
 # LEDE源码通用diy2.sh文件
-
+################################################################################################################
 Diy_lede2() {
 echo "LEDE源码自定义2"
 }
@@ -56,12 +53,9 @@ echo "LEDE源码自定义2"
 ################################################################################################################
 
 
-
 ################################################################################################################
-
-
 # LIENOL源码通用diy1.sh文件
-
+################################################################################################################
 Diy_lienol() {
 echo "LIENOL源码自定义1"
 cp -Rf build/common/LIENOL/* "${PATH1}"
@@ -75,13 +69,12 @@ git clone https://github.com/jerrykuku/luci-app-vssr package/danshui/luci-app-vs
 git clone https://github.com/vernesong/OpenClash package/danshui/luci-app-openclash
 git clone https://github.com/frainzy1477/luci-app-clash package/danshui/luci-app-clash
 git clone https://github.com/garypang13/luci-app-bypass package/danshui/luci-app-bypass
-
 find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-redir/shadowsocksr-libev-alt/g' {}
 find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-server/shadowsocksr-libev-server/g' {}
 }
-
+################################################################################################################
 # LIENOL源码通用diy2.sh文件
-
+################################################################################################################
 Diy_lienol2() {
 echo "LIENOL源码自定义2"
 }
@@ -89,12 +82,9 @@ echo "LIENOL源码自定义2"
 ################################################################################################################
 
 
-
 ################################################################################################################
-
-
 # 天灵源码通用diy1.sh文件
-
+################################################################################################################
 Diy_immortalwrt() {
 echo "天灵源码自定义1"
 cp -Rf build/common/PROJECT/* "${PATH1}"
@@ -110,17 +100,18 @@ git clone https://github.com/garypang13/luci-app-bypass package/danshui/luci-app
 
 }
 
+################################################################################################################
 # 天灵源码通用diy2.sh文件
-
+################################################################################################################
 Diy_immortalwrt2() {
 echo "天灵源码自定义2"
 }
-
 ################################################################################################################
 
 
+################################################################################################################
 # N1、微加云、贝壳云、我家云、S9xxx 打包程序
-
+################################################################################################################
 Diy_n1() {
 cd ../
 svn co https://github.com/281677160/N1/trunk reform
@@ -131,12 +122,12 @@ sudo ./gen_openwrt -d -k latest
 devices=("phicomm-n1" "rk3328" "s9xxx" "vplus")
 }
 
-
 ################################################################################################################
 
 
+################################################################################################################
 # 公告
-
+################################################################################################################
 Diy_notice() {
 echo ""
 echo "	《公告内容》"
@@ -144,8 +135,6 @@ echo " 祝大家天天快乐、生活愉快！"
 echo " 使用中有疑问的可以加入电报群，跟群友交流"
 echo ""
 }
-
-
 Diy_xinxi_Base() {
 GET_TARGET_INFO
 if [[ "${TARGET_PROFILE}" =~ (x86-64|phicomm-k3|d-team_newifi-d2|phicomm_k2p|k2p|phicomm_k2p-32m) ]]; then
@@ -166,6 +155,16 @@ echo " 源码作者: ${ZUOZHE}"
 echo " 编译机型: ${TARGET_PROFILE}"
 echo " 固件作者: ${Author}"
 echo " 仓库地址: ${Github_Repo}"
+if [[ ${UPLOAD_BIN_DIR} == "true" ]]; then
+	echo " 上传BIN文件夹(固件+IPK): 开启"
+else
+	echo " 上传BIN文件夹(固件+IPK): 关闭"
+fi
+if [[ ${UPLOAD_CONFIG} == "true" ]]; then
+	echo " 上传[.config]配置文件: 开启"
+else
+	echo " 上传[.config]配置文件: 关闭"
+fi
 if [[ ${UPLOAD_FIRMWARE} == "true" ]]; then
 	echo " 上传固件在github空间: 开启"
 else
