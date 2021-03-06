578a30785e326fe0105c874a06cb784270da1017
281677160 <66651997+281677160@users.noreply.github.com>
2021-03-07 01:21:04 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -137,7 +137,7 @@ fi
 
 
 Diy_adguard() {
-ADGU="$(grep "CONFIG_PACKAGE_luci-app-adguardhome" ${Home}/.config)"
+ADGU="$(grep "CONFIG_PACKAGE_luci-app-adguardhome=y"  .config)"
 if [[ "${ADGU}" == "CONFIG_PACKAGE_luci-app-adguardhome=y" ]];then
 	if [[ "${TARGET_PROFILE}" == "x86-64" ]];then
 		svn co https://github.com/281677160/ceshi1/branches/AdGuard/x86-adgu files
