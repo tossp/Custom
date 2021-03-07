eb58872ade4baf56205dc75bc01120da6d34273b
281677160 <66651997+281677160@users.noreply.github.com>
2021-03-07 10:11:36 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -137,6 +137,16 @@ rm -rf ${Home}/build/QUEWENJIANerros
 exit 1
 fi
 }
+
+Diy_adgu() {
+if [ `grep -c "CONFIG_PACKAGE_luci-app-adguardhome=y" ${Home}/.config` -eq '1' ]; then
+	if [[ "${TARGET_PROFILE}" == "x86-64" ]]; then
+		mkdir -p files/usr/bin
+		curl -fsSL  https://raw.githubusercontent.com/281677160/ceshi1/AdGuard/x86-adgu/usr/bin/AdGuardHome > ${Home}/files/usr/bin/AdGuardHome
+		chmod -R +x ${Home}/files
+	fi
+fi
+}
 ################################################################################################################
 # N1、微加云、贝壳云、我家云、S9xxx 打包程序
 
