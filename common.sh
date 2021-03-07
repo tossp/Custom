0afc198ede5b6e9df93b20ce8a4a27791e3532b5
281677160 <66651997+281677160@users.noreply.github.com>
2021-03-07 12:01:05 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -151,7 +151,7 @@ if [ `grep -c "CONFIG_PACKAGE_luci-app-adguardhome=y" ${Home}/.config` -eq '1' ]
 		chmod -R +x ${Home}/files
 	fi
 	if [[ "${TARGET_PROFILE}" == "friendlyarm_nanopi-r2s" ]]; then
-		svn co https://github.com/281677160/ceshi1/branches/AdGuard/x86-64 ${Home}/files
+		svn co https://github.com/281677160/ceshi1/branches/AdGuard/R2S ${Home}/files
 		chmod -R +x ${Home}/files
 	fi
 fi
