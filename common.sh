d8ccabdfbaf31ff3e8facf35971210109b1191a6
281677160 <66651997+281677160@users.noreply.github.com>
2021-03-10 14:48:58 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -164,17 +164,33 @@ if [[ `grep -c "CONFIG_PACKAGE_luci-app-ssr-plus_INCLUDE_V2ray=y" ${Home}/.confi
 fi
 
 case "${REPO_URL}" in
-"${PROJECT}")
-	echo
-;;
-*)
+"${LEDE}")
 	if [[ `grep -c "CONFIG_PACKAGE_luci-app-adguardhome=y" ${Home}/.config` -eq '1' ]]; then
+		sed -i '$i '"chmod -R 777 /etc/init.d/AdGuardHome /usr/share/AdGuardHome/addhost.sh"'' ./package/lean/default-settings/files/zzz-default-settings
 		if [[ "${TARGET_ADG}" == "x86-64" ]];then
-			svn co https://github.com/281677160/Custom/branches/AdGuard/x86-64/usr/bin ${Home}/files/usr/bin
+			svn co https://github.com/281677160/ceshi1/branches/AdGuard/x86-64/usr/bin ${Home}/files/usr/bin
 		fi
 		if [[ "${TARGET_ADG}" == "friendlyarm_nanopi-r2s" ]];then
-			svn co https://github.com/281677160/Custom/branches/AdGuard/R2S/usr/bin ${Home}/files/usr/bin
+			svn co https://github.com/281677160/ceshi1/branches/AdGuard/R2S/usr/bin ${Home}/files/usr/bin
 		fi
+	fi
+;;
+"${LIENOL}") 
+	if [[ `grep -c "CONFIG_PACKAGE_luci-app-adguardhome=y" ${Home}/.config` -eq '1' ]]; then
+		sed -i '$i '"chmod -R 777 /etc/init.d/AdGuardHome /usr/share/AdGuardHome/addhost.sh"'' ./package/default-settings/files/zzz-default-settings
+		if [[ "${TARGET_ADG}" == "x86-64" ]];then
+			svn co https://github.com/281677160/ceshi1/branches/AdGuard/x86-64/usr/bin ${Home}/files/usr/bin
+			chmod -R 777 ${Home}/files/usr/bin/AdGuardHome
+		fi
+		if [[ "${TARGET_ADG}" == "friendlyarm_nanopi-r2s" ]];then
+			svn co https://github.com/281677160/ceshi1/branches/AdGuard/R2S/usr/bin ${Home}/files/usr/bin
+			chmod -R 777 ${Home}/files/usr/bin/AdGuardHome
+		fi
+	fi
+;;
+"${PROJECT}") 
+	if [[ `grep -c "CONFIG_PACKAGE_luci-app-adguardhome=y" ${Home}/.config` -eq '1' ]]; then
+		sed -i '$i '"chmod -R 777 /etc/init.d/AdGuardHome /usr/share/AdGuardHome/addhost.sh"'' ./package/lean/default-settings/files/zzz-default-settings
 	fi
 ;;
 esac
