f366402bcc879ff2785fe175562e552d7fd82402
281677160 <66651997+281677160@users.noreply.github.com>
2021-03-13 11:17:35 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -238,6 +238,9 @@ grep -i CONFIG_PACKAGE_luci-app .config | grep  -v \# >Plug-in
 sed -i "s/=y//g" Plug-in
 sed -i "s/CONFIG_PACKAGE_//g" Plug-in
 sed -i '/INCLUDE/d' Plug-in > /dev/null 2>&1
+cat -n Plug-in > Plugin
+sed -i 's/	luci/、luci/g' Plugin
+awk '{print "      " $0}' Plugin > Plug-in
 
 if [ `grep -c "CONFIG_TARGET_x86_64=y" ${Home}/.config` -eq '1' ]; then
 	TARGET_ADG="x86-64"
@@ -333,7 +336,7 @@ fi
 if [[ "${Modelfile}" =~ (Lede_phicomm_n1|Project_phicomm_n1) ]]; then
 	TARGET_PROFILE="N1,Vplus,Beikeyun,L1Pro,S9xxx"
 fi
-echo ""
+echo
 echo " 编译源码: ${COMP2}"
 echo " 源码链接: ${REPO_URL}"
 echo " 源码分支: ${REPO_BRANCH}"
@@ -341,20 +344,24 @@ echo " 源码作者: ${ZUOZHE}"
 echo " 编译机型: ${TARGET_PROFILE}"
 echo " 固件作者: ${Author}"
 echo " 仓库地址: ${Github_Repo}"
-if [[ ${UPLOAD_BIN_DIR} == "true" ]]; then
-	echo " 上传BIN文件夹(固件+IPK): 开启"
+echo " 启动编号: #${Run_number}（本仓库第${Run_number}次启动[${Run_workflow}]工作流程）"
+echo " 编译时间: $(TZ=UTC-8 date "+%Y年%m月%d号-%H时%M分")"
+echo " 您当前使用【${Modelfile}】文件夹编译【${TARGET_PROFILE}】固件"
+echo
+if [[ ${UPLOAD_FIRMWARE} == "true" ]]; then
+	echo " 上传固件在github actions: 开启"
 else
-	echo " 上传BIN文件夹(固件+IPK): 关闭"
+	echo " 上传固件在github actions: 关闭"
 fi
 if [[ ${UPLOAD_CONFIG} == "true" ]]; then
 	echo " 上传[.config]配置文件: 开启"
 else
 	echo " 上传[.config]配置文件: 关闭"
 fi
-if [[ ${UPLOAD_FIRMWARE} == "true" ]]; then
-	echo " 上传固件在github actions: 开启"
+if [[ ${UPLOAD_BIN_DIR} == "true" ]]; then
+	echo " 上传BIN文件夹(固件+IPK): 开启"
 else
-	echo " 上传固件在github actions: 关闭"
+	echo " 上传BIN文件夹(固件+IPK): 关闭"
 fi
 if [[ ${UPLOAD_COWTRANSFER} == "true" ]]; then
 	echo " 上传固件到到【奶牛快传】和【WETRANSFER】: 开启"
@@ -379,8 +386,8 @@ fi
 if [[ ${SSHYC} == "true" ]]; then
 	echo " SSH远程连接临时开关: 开启"
 fi
+echo
 if [[ ${REGULAR_UPDATE} == "true" ]]; then
-	echo
 	echo " 把定时自动更新插件编译进固件: 开启"
 	echo " 插件版本: ${AutoUpdate_Version}"
 	echo " 固件名称: ${Firmware_mz}"
@@ -398,8 +405,6 @@ else
 	echo " 把定时自动更新插件编译进固件: 关闭"
 	echo
 fi
-echo " * 您当前使用的是【${Modelfile}】文件夹编译【${TARGET_PROFILE}】固件,编译时间 $(TZ=UTC-8 date "+%Y年%m月%d日")！*"
-echo
 echo " 系统空间      类型   总数  已用  可用 使用率"
 cd ../ && df -hT $PWD && cd openwrt
 echo
@@ -409,9 +414,9 @@ if [ -n "$(ls -A "${Home}/Chajianlibiao" 2>/dev/null)" ]; then
 fi
 if [ -n "$(ls -A "${Home}/Plug-in" 2>/dev/null)" ]; then
 	echo
-	echo "	   已选插件列表"
-	[ -s Plug-in ] && cat -n Plug-in
+	echo "		已选插件列表"
+	[ -s Plug-in ] && cat Plug-in
 	echo
 fi
-rm -rf {CHONGTU,Plug-in,Chajianlibiao}
+rm -rf {CHONGTU,Plug-in,Plugin,Chajianlibiao}
 }
